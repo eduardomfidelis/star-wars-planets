705a27575f13764bbf043447273db7d1a6589bb9
eduardo maciel <eduardomacielfidelis@gmail.com>
2023-09-19 18:49:20 -0300

    requisito 3 - adiciona filtros e seletores

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import Table from './Table';
 
 function App() {
   const [planets, setPlanets] = useState([]);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
   const [nameFilter, setNameFilter] = useState('');
+  const [columnFilter, setColumnFilter] = useState('population');
+  const [comparisonFilter, setComparisonFilter] = useState('maior que');
+  const [valueFilter, setValueFilter] = useState('0');
 
   useEffect(() => {
     async function fetchPlanets() {
@@ -15,23 +19,101 @@ function App() {
     fetchPlanets();
   }, []);
 
-  const planetsNameFilter = nameFilter.trim() === '' ? planets : planets
-    .filter((planet: any) => planet.name.toLowerCase()
-      .includes(nameFilter.toLowerCase()));
+  useEffect(() => {
+    Filters();
+  });
 
-  const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+  const Filters = () => {
+    let filteredData = planets;
+
+    if (nameFilter.trim() !== '') {
+      filteredData = filteredData
+        .filter((planet: any) => planet.name.toLowerCase()
+          .includes(nameFilter.toLowerCase()));
+    }
+
+    filteredData = filteredData.filter((planet) => {
+      const planetValue = Number(planet[columnFilter]);
+
+      switch (comparisonFilter) {
+        case 'maior que':
+          return planetValue > Number(valueFilter);
+        case 'menor que':
+          return planetValue < Number(valueFilter);
+        case 'igual a':
+          return planetValue === Number(valueFilter);
+        default:
+          return true;
+      }
+    });
+
+    setFilteredPlanets(filteredData);
+  };
+
+  const handleNameFilterChange = (event: any) => {
     setNameFilter(event.target.value);
   };
+
+  const handleColumnFilterChange = (event: any) => {
+    setColumnFilter(event.target.value);
+  };
+
+  const handleComparisonFilterChange = (event: any) => {
+    setComparisonFilter(event.target.value);
+  };
+
+  const handleValueFilterChange = (event: any) => {
+    setValueFilter(event.target.value);
+  };
+
+  const handleFilterButtonClick = () => {
+    Filters();
+  };
+
   return (
     <div>
       <input
         type="text"
         value={ nameFilter }
-        onChange={ handleChange }
+        onChange={ handleNameFilterChange }
         data-testid="name-filter"
+        placeholder="Filtrar por nome"
       />
-      <Table planets={ planetsNameFilter } />
 
+      <select
+        value={ columnFilter }
+        onChange={ handleColumnFilterChange }
+        data-testid="column-filter"
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+
+      <select
+        value={ comparisonFilter }
+        onChange={ handleComparisonFilterChange }
+        data-testid="comparison-filter"
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+
+      <input
+        type="text"
+        value={ valueFilter }
+        onChange={ handleValueFilterChange }
+        data-testid="value-filter"
+      />
+
+      <button data-testid="button-filter" onClick={ handleFilterButtonClick }>
+        Filtrar
+      </button>
+
+      <Table planets={ filteredPlanets.length > 0 ? filteredPlanets : planets } />
     </div>
   );
 }
